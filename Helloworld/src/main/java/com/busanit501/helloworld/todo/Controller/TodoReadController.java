ee05cc1401d5d6c9859f5faf19fb24494b022ede
“Ihan0316” <aijnjj12@gmail.com>
2024-11-21 16:01:40 +0900

    수정

--- Helloworld/src/main/java/com/busanit501/helloworld/todo/Controller/TodoReadController.java
+++ Helloworld/src/main/java/com/busanit501/helloworld/todo/Controller/TodoReadController.java
@@ -1,5 +1,8 @@
 package com.busanit501.helloworld.todo.Controller;
 
+import com.busanit501.helloworld.todo.DTO.TodoDTO;
+import com.busanit501.helloworld.todo.Service.TodoService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,9 +14,15 @@ import java.io.IOException;
 public class TodoReadController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        System.out.println("doGet TodoReadController 하나 조회 예시");
+        // 클릭한 게시글 번호를 가지고 와야함
+        Long tno = Long.parseLong(request.getParameter("tno"));
+
         // 서비스에서 todo 더미 데이터를 조회 후,
+        TodoDTO todoDTO = TodoService.INSTANCE.getOne(tno);
 
         // 화면에 전달하기
-
+        request.setAttribute("dto", todoDTO);
+        request.getRequestDispatcher("/WEB-INF/todo/todoRead.jsp").forward(request, response);
     }
 }
--- Helloworld/src/main/webapp/WEB-INF/todo/toRead.jsp
+++ Helloworld/src/main/webapp/WEB-INF/todo/toRead.jsp
@@ -8,9 +8,12 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>Todo Read</title>
 </head>
 <body>
-
+    <h1>tno 번호로 하나 조회하는 화면, 상세보기와 같음</h1>
+    tno : ${dto.tno}
+    title : ${dto.title}
+    dueDate : ${dto.dueDate}
 </body>
 </html>
