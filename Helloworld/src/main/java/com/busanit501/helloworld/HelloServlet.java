8f1d353b65a69e85b606a1d6c4c29f409281e8ca
“Ihan0316” <aijnjj12@gmail.com>
2024-11-21 10:16:37 +0900

    2교시 생명주기 확인

--- Helloworld/src/main/java/com/busanit501/helloworld/HelloServlet.java
+++ Helloworld/src/main/java/com/busanit501/helloworld/HelloServlet.java
@@ -9,11 +9,14 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
+        // 생명주기 확인용 콘솔 출력(1번 출력됨)
+        System.out.println("HelloServlet init 1번 호출");
         message = "Hello World!";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
+        System.out.println("HelloServlet doGet 매번 호출됨");
 
         // Hello
         PrintWriter out = response.getWriter();
@@ -25,5 +28,6 @@ public class HelloServlet extends HttpServlet {
     }
 
     public void destroy() {
+        System.out.println("HelloServlet destroy 1번 호출");
     }
 }
