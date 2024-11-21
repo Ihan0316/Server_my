4b91df9576c589dc3b7a9a73259a1d9cc855b59b
“Ihan0316” <aijnjj12@gmail.com>
2024-11-21 10:02:15 +0900

    수정

--- Helloworld/src/main/java/com/busanit501/helloworld/Login/Login_resultController.java
+++ Helloworld/src/main/java/com/busanit501/helloworld/Login/Login_resultController.java
@@ -1,6 +1,5 @@
 package com.busanit501.helloworld.Login;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
