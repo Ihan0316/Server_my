ac3ffcc90480272a849e1e13f803e778d467b5f3
“Ihan0316” <aijnjj12@gmail.com>
2024-11-22 14:24:14 +0900

    수정

---
+++ Helloworld/src/main/java/com/busanit501/helloworld/JDBCex/dao/TodoDAO.java
@@ -0,0 +1,67 @@
+package com.busanit501.helloworld.JDBCex.dao;
+
+import com.busanit501.helloworld.JDBCex.dto.TodoVO;
+import lombok.Cleanup;
+
+import java.sql.*;
+
+public class TodoDAO {
+
+    //Todo 등록 기능, 추가하기
+    // VO (value object) 실제 디비 컬럼과 일치함
+    // 서비스 계층에서 vo넘겨 받은 데이터 중에서, 보여줄 데이터만 따로 분리해서
+    // 전달하는 용도로 사용하는 DTO 이다.
+
+    public void insert(TodoVO todoVO) throws SQLException {
+
+        String sql = "insert into tbl_todo(title, dueDate, finished)" + "values(?,?,?)";
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        @Cleanup PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, todoVO.getTitle());
+        preparedStatement.setDate(2, Date.valueOf(todoVO.getDueDate()));
+        preparedStatement.setBoolean(3, todoVO.isFinished());
+        preparedStatement.executeUpdate();
+    } // insert
+
+    public String getTime() {
+        String now = null;
+        // hikariCP 이용해서,
+        // 디비 연결하고,
+        // sql 전달하고,
+        // 결과값 받고,
+        // 자원 반납
+        // 자원 반납 하는 방법 2가지.
+        //1)
+        // try catch -> try with resource , 자동으로 자원 반납을 함.
+        // autocloseable 인터페이스를 구현한 기능들만, 자동 반납.
+        // 2) 애너테이션 이용해서, @cleanup , 이용하면, 간단히 자동 반납.
+        try (Connection connection = ConnectionUtil.INSTANCE.getConnection();
+             PreparedStatement preparedStatement =
+                     connection.prepareStatement("select now()");
+             ResultSet resultSet = preparedStatement.executeQuery();
+        ) {
+            resultSet.next();
+            now = resultSet.getString(1);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } //catch
+        return now;
+    } //getTime
+
+    public String getTime2() throws SQLException {
+        String now = null;
+        // 자동으로 DB의 커넥션을 반납하는 방법2
+        //@cleanup
+        @Cleanup Connection connection = ConnectionUtil.INSTANCE.getConnection();
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("select now()");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        now = resultSet.getString(1);
+        return now;
+    }
+
+
+} //class
+
---
+++ Helloworld/src/test/java/com/busanit501/helloworld/jdbcex/TodoDAOTest.java
@@ -0,0 +1,27 @@
+package com.busanit501.helloworld.jdbcex;
+
+import com.busanit501.helloworld.JDBCex.dao.TodoDAO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+public class TodoDAOTest {
+    private TodoDAO todoDAO;
+
+    // 아래에 각 단위 테스트가 실행되기전에, 먼저 실행을 함.
+    @BeforeEach
+    public void ready() {
+        todoDAO = new TodoDAO();
+    }
+
+    @Test
+    public void getTime() {
+        System.out.println("sql 전달 후, 시간 조회 확인용: "+todoDAO.getTime());
+    }
+
+    @Test
+    public void getTime2() throws SQLException {
+        System.out.println("sql 전달 후, 시간 조회 확인용 자동 반납 @Cleanup: "+todoDAO.getTime2());
+    }
+}
