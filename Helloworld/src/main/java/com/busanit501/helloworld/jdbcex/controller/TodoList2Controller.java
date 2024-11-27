6cec1b1632091e09328a37ececfeefced8339273
“Ihan0316” <aijnjj12@gmail.com>
2024-11-27 14:58:32 +0900

    24/11/27 6교시 수정

--- Helloworld/src/main/java/com/busanit501/helloworld/jdbcex/controller/TodoList2Controller.java
+++ Helloworld/src/main/java/com/busanit501/helloworld/jdbcex/controller/TodoList2Controller.java
@@ -10,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
@@ -22,8 +23,28 @@ public class TodoList2Controller extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 세션정보 호출 및 가져오기
+        HttpSession session = request.getSession();
 
-        //
+        // 만약 서버에서 최초로 접근을 했다면
+        // 서버에서 JSESSIONID를 발급
+        if(session.isNew()){
+            log.info("최초로 서버에 요청을 함");
+            // 로그인 컨트롤러가 아직 없음.
+            response.sendRedirect("/login");
+            return;
+        }
+        // 2번째 이후의 방문, 세션이라는 저장공간
+        // 키 : loginInfo, 값 : 로그인한 유저의 아이디를 기록
+        if(session.getAttribute("loginInfo") == null){
+            log.info("2번째 이후로 서버에 요청을 했고, 로그인 정보는 없는 경우");
+            // 로그인 컨트롤러가 아직 없음.
+            response.sendRedirect("/login");
+            return;
+        }
+        // 임시로, 최초도 아니고, 로그인 처리가 되었다면
+        // 정상적으로 접근하는 페이지로 이동 시켜 줄게
+
         log.info("doGet TodoList2Controller 확인");
         try {
             // 서비스에 외주 주고, 전체 목록 리스트 받아오기.
