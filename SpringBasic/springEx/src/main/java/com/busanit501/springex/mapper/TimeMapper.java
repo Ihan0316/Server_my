f9f59b95cc669e8c82975d3457ec04fe2a83fea9
“Ihan0316” <aijnjj12@gmail.com>
2024-12-02 11:47:21 +0900

    24/12/2 3교시

--- SpringBasic/springEx/src/main/java/com/busanit501/springex/mapper/TimeMapper.java
+++ SpringBasic/springEx/src/main/java/com/busanit501/springex/mapper/TimeMapper.java
@@ -1,4 +1,12 @@
 package com.busanit501.springex.mapper;
 
+import org.apache.ibatis.annotations.Select;
+
 public interface TimeMapper {
+    // 방법1, 맵퍼 인터페이스에서 어노테이션 이용해서 디비 호출
+    @Select("select now()")
+    String getNow(); // 추상메서드
+
+    // 방법2, sql문을 xml파일로 분리해서 이용
+
 }
