691359d0bf0a7bb6b20868142a460dfd03c632da
“Ihan0316” <aijnjj12@gmail.com>
2024-12-02 09:48:54 +0900

    24/12/2 1교시

--- SpringBasic/springEx/src/main/java/com/busanit501/springex/sample/SampleService.java
+++ SpringBasic/springEx/src/main/java/com/busanit501/springex/sample/SampleService.java
@@ -2,7 +2,6 @@ package com.busanit501.springex.sample;
 
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -12,19 +11,19 @@ import org.springframework.stereotype.Service;
 @Component
 @Service
 @ToString
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 public class SampleService {
     // 방법1, 필드 주입방식
 //     @Autowired
 //     private SampleDAO sampleDAO;
 
     // 방법2, 생성자 주입 방식 -> 1) 생성자로 주입, 2) 롬복 애너테이션 이용
-//     private final SampleDAO sampleDAO;
     // 1) 생성자로 주입
 //     public SampleService(SampleDAO sampleDAO) {
 //     this.sampleDAO = sampleDAO;
-     }
 
     // 2) 롬복 애너테이션 이용
+     private final SampleDAO sampleDAO;
+     }
 
 
