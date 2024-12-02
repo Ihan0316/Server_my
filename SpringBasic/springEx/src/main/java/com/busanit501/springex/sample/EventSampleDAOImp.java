198ae7b68256f4849d0082934364cd991e7d1202
“Ihan0316” <aijnjj12@gmail.com>
2024-12-02 09:50:11 +0900

    24/12/2 1교시

--- SpringBasic/springEx/src/main/java/com/busanit501/springex/sample/EventSampleDAOImp.java
+++ SpringBasic/springEx/src/main/java/com/busanit501/springex/sample/EventSampleDAOImp.java
@@ -1,4 +1,9 @@
 package com.busanit501.springex.sample;
 
-public class EventSampleDAOImp {
+import org.springframework.stereotype.Repository;
+
+// 문제점, SampleDAO, 인터페이스를 구현하는 클래스가 2개
+// 시스템에 2개를 알려줌, 어느 클래스를 구현해야 하는지, 고민이 생김.
+@Repository
+public class EventSampleDAOImp implements SampleDAO{
 }
