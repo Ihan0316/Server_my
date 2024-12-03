d16aa52cafeb23a95e0702cf9675224bdd3fb1fd
“Ihan0316” <aijnjj12@gmail.com>
2024-12-03 11:23:12 +0900

    24/12/3 3교시

--- SpringBasic/springEx/src/main/java/com/busanit501/springex/controller/SampleController.java
+++ SpringBasic/springEx/src/main/java/com/busanit501/springex/controller/SampleController.java
@@ -1,10 +1,12 @@
 package com.busanit501.springex.controller;
 
+import com.busanit501.springex.dto.TodoDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
 
@@ -12,49 +14,49 @@ import java.time.LocalDate;
 @Log4j2
 public class SampleController {
 
-    @GetMapping("/hello")
     // 확인은 http://localhost:8080/hello
     // 맵핑이 매서드명과 동일한 뷰 파일로 연결
     // /WEB-INF/views/
     // hello(메서드명)
     // .jsp 확장자
+    @GetMapping("/hello")
     public void hello() {
         // 아직 화면이 없음, 임의로 설정
         log.info("hello");
     }
 
-    @GetMapping("/hello2")
     // 리턴타입이 문자열이면, 해당 문자열의 이름이 뷰의 파일명으로 뷰를 맵핑, 할당
+    @GetMapping("/hello2")
     public String hello2() {
         // 아직 화면이 없음, 임의로 설정
         log.info("hello2");
         return "helloTest";
     }
 
-    @GetMapping("/ex1")
     // 파라미터 수집 여부 확인, 콘솔에서 확인
     // localhost:8080/ex1?name=ihan&age=26
+    @GetMapping("/ex1")
     public void ex1(String name, int age) {
         log.info("ex1 name : " + name);
         log.info("ex1 age : " + age);
     }
 
-    @GetMapping("/ex2")
     // 파라미터 수집 여부 확인, 콘솔에서 확인
     // localhost:8080/ex2?name=ihan&age=26
+    @GetMapping("/ex2")
     public void ex2(@RequestParam(name = "name", defaultValue = "JIH") String name,
                     @RequestParam(name = "age", defaultValue = "30") int age) {
         log.info("ex2 name : " + name);
         log.info("ex2 age : " + age);
     }
 
-    @GetMapping("/ex3")
     // 웹브라우저에서 넘어온 데이터 타입은 문자열
     // 받을때 타입 불일치 오류 확인
     // localhost:8080/ex3?dueDate=2024-12-03
     // 대책 -> 미리 형이 다른 문자열에 대해 localdate를 파싱,string타입으로
     // formatter 이용, 파일 분리, 시스템에 빈 등록
     // LocalDate가 문자열로 전송되어도, 시스템이 자동으로 LocalDate로 형변환 해줌
+    @GetMapping("/ex3")
     public void ex3(LocalDate dueDate) {
         log.info("ex3 dueDate : " + dueDate);
     }
@@ -67,5 +69,31 @@ public class SampleController {
         model.addAttribute("msg"," <script>\n" +
                 "                    alert('이것은 JavaScript alert 테스트입니다!, 만약, 공격자가 악성 코드를 이런식으로 문자열에 포함하면 안 좋은일이 생김');\n" +
                 "                </script>");
+    }
+
+    // 웹브라우저에서 TodoDTO 멤버 타입 형식으로 받고
+    // 다시 서버 -> 웹으로 전달
+    // 파라미터, TodoDTO todoDTO 선언되어 있으면,
+    // 화면에서 그대로 사용가능 ${todoDTO}
+    // Model model 사용 안해도, 스프링 프레임워크에서 자동으로 화면에서 사용가능
+    // localhost:8080/ex5?title=lsy&writer=이상용
+    @GetMapping("/ex5")
+    public void ex5(TodoDTO todoDTO, Model model) {
+        log.info("ex5 : " + todoDTO);
+    }
+
+    // 리다이렉트시 데이터 전달
+    // 1) 키:값으로 데이터 전달
+    // 2) 일화용으로 데이터 전달 예시
+    @GetMapping("/ex6")
+    public String ex6(RedirectAttributes redirectAttributes) {
+        log.info("ex6 : " + redirectAttributes);
+        // 키:값으로 데이터 전달
+        // 서버 -> 웹으로 전달
+        // 화면에서 데이터 탑재 후 전달
+        redirectAttributes.addAttribute("msg", "잘 보고이써여");
+        redirectAttributes.addFlashAttribute("msg2", "일회용으로 사용");
+
+        return "redirect:/ex7";
     }
 }
---
+++ SpringBasic/springEx/src/main/webapp/WEB-INF/views/ex5.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ihanjo
+  Date: 24. 12. 3.
+  Time: 오전 10:47
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>ex5, 서버로 넘어온 더미 데이터 확인, 모델 객체 없이 스프링에서 파라미터 이름으로 넘어온 데이터를 사용</h1>
+<h2>넘어온 데이터 확인 todoDTO : <c:out value="${todoDTO}"/></h2>
+</body>
+</html>
