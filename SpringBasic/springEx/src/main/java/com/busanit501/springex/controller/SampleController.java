8e558fe4a31835b5afd61532df91b739c06130cd
“Ihan0316” <aijnjj12@gmail.com>
2024-12-03 10:21:41 +0900

    24/12/3 2교시

--- SpringBasic/springEx/src/main/java/com/busanit501/springex/controller/SampleController.java
+++ SpringBasic/springEx/src/main/java/com/busanit501/springex/controller/SampleController.java
@@ -3,6 +3,9 @@ package com.busanit501.springex.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
 
 @Controller
 @Log4j2
@@ -26,4 +29,33 @@ public class SampleController {
         log.info("hello2");
         return "helloTest";
     }
+
+    @GetMapping("/ex1")
+    // 파라미터 수집 여부 확인, 콘솔에서 확인
+    // localhost:8080/ex1?name=ihan&age=26
+    public void ex1(String name, int age) {
+        log.info("ex1 name : " + name);
+        log.info("ex1 age : " + age);
+    }
+
+    @GetMapping("/ex2")
+    // 파라미터 수집 여부 확인, 콘솔에서 확인
+    // localhost:8080/ex2?name=ihan&age=26
+    public void ex2(@RequestParam(name = "name", defaultValue = "JIH") String name,
+                    @RequestParam(name = "age", defaultValue = "30") int age) {
+        log.info("ex2 name : " + name);
+        log.info("ex2 age : " + age);
+    }
+
+    @GetMapping("/ex3")
+    // 웹브라우저에서 넘어온 데이터 타입은 문자열
+    // 받을때 타입 불일치 오류 확인
+    // localhost:8080/ex3?dueDate=2024-12-03
+    // 대책 -> 미리 형이 다른 문자열에 대해 localdate를 파싱,string타입으로
+    public void ex3(LocalDate dueDate) {
+        log.info("ex3 dueDate : " + dueDate);
+    }
+
+    //formatter 이용
+
 }
---
+++ SpringBasic/springEx/src/main/java/com/busanit501/springex/controller/formatter/LocalDateFormatter.java
@@ -0,0 +1,24 @@
+package com.busanit501.springex.controller.formatter;
+
+import org.springframework.format.Formatter;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+// LocalDate와 String 간의 미리 형변환 작업 기능
+public class LocalDateFormatter implements Formatter<LocalDate> {
+
+    @Override
+    public LocalDate parse(String text, Locale locale) throws ParseException {
+        LocalDate localDate = LocalDate.parse(text, DateTimeFormatter.ofPattern("dd/MM/yyyy", locale));
+        return localDate;
+    }
+
+    @Override
+    public String print(LocalDate object, Locale locale) {
+        String formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd").format(object);
+        return formatter;
+    }
+}
