5409ce22af163d9bb1d914ef8df3360567055ce4
“Ihan0316” <aijnjj12@gmail.com>
2024-12-03 10:01:38 +0900

    24/12/3 1교시

--- SpringBasic/springEx/src/main/java/com/busanit501/springex/controller/TodoController.java
+++ SpringBasic/springEx/src/main/java/com/busanit501/springex/controller/TodoController.java
@@ -3,13 +3,28 @@ package com.busanit501.springex.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller // 화면, 데이터 제공
 @RequestMapping("/todo") // 경로지정, /todo 경로로 오는 모든 url은 이 컨트롤ㄹ가 받아서 작업함
 @Log4j2
 public class TodoController {
+    // localhost:8080/todo/list
     @RequestMapping("/list")
     public void list() {
         log.info("TodoController list : 화면제공은 해당 메서드 명으로 제공함");
+    }
+
+    // localhost:8080/todo/register
+    // 1) 글작성 폼 -> get
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
+    public void register() {
+        log.info("TodoController register : 화면제공은 해당 메서드 명으로 제공함");
+    }
+
+    // 2) 글작성 로직 처리 -> post
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public void registerPost() {
+        log.info("TodoController register post 로직처리");
     }
 }
---
+++ SpringBasic/springEx/src/main/webapp/WEB-INF/views/todo/register.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ihanjo
+  Date: 24. 12. 2.
+  Time: 오후 3:14
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>/todo/register, register.jsp 화면입니다.</h1>
+<form action="/todo/register" method="post">
+    <button type="submit">글작성</button>
+</form>
+</body>
+</html>
