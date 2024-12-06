f7acdce595fcefe854ebeb386dba35a3a04f0ab2
“Ihan0316” <aijnjj12@gmail.com>
2024-12-06 10:01:24 +0900

    미니실습 완료

--- Minitest/src/test/java/com/busanit501/mapper/FoodMapperTest.java
+++ Minitest/src/test/java/com/busanit501/mapper/FoodMapperTest.java
@@ -1,6 +1,7 @@
 package com.busanit501.mapper;
 
 import com.busanit501.minitest.domain.FoodVO;
+import com.busanit501.minitest.dto.PageRequestDTO;
 import com.busanit501.minitest.mapper.FoodMapper;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
@@ -54,5 +55,43 @@ public class FoodMapperTest {
     @Test
     public void testDelete() {
         foodMapper.delete(6L);
+    }
+
+    @Test
+    public void testUpdate() {
+        // 업데이트 할 더미 데이터 필요, TodoVO
+        FoodVO foodVO = FoodVO.builder()
+                .fno(6L)
+                .foodName("수정 제목")
+                .dueDate(LocalDate.now())
+                .finished(true)
+                .build();
+
+        foodMapper.update(foodVO);
+    }
+
+    // 페이징 처리해서 전체 조회
+    @Test
+    public void testSelectAllWithPage() {
+        // 페이징 준비물을 담은 PageRequestDTO 필요함,
+        // 더미로 PageRequestDTO 만들고,
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(2)
+                .size(10)
+                .build();
+
+        List<FoodVO> list = foodMapper.selectList(pageRequestDTO);
+        list.forEach(vo -> log.info("vo : " + vo));
+    }
+
+    // 페이징 처리해서 전체 갯수 조회
+    @Test
+    public void testGetCount() {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .build();
+        int total = foodMapper.getCount(pageRequestDTO);
+        log.info("total : " + total);
     }
 }
