e61ae2ed45700bec0fa75f6f6655cac6e19a7126
“Ihan0316” <aijnjj12@gmail.com>
2024-12-09 16:03:12 +0900

    24/12/09 7교시

---
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/controller/SampleController.java
@@ -0,0 +1,17 @@
+package com.busanit501.boot501.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Log4j2
+public class SampleController {
+    @GetMapping("/hello")
+    // 레거시에서 앞단은 jsp 사용
+    // 부트에서는 화면을 타임리프 사용, 확장자 - .html 동일함
+    public void hello(Model model) {
+        model.addAttribute("msg", "hello world!");
+    }
+}
