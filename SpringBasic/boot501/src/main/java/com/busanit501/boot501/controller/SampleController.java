8e580e22281bd15d4334011ce30dcaa1f3d30543
“Ihan0316” <aijnjj12@gmail.com>
2024-12-09 16:33:21 +0900

    24/12/09 7교시

--- SpringBasic/boot501/src/main/java/com/busanit501/boot501/controller/SampleController.java
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/controller/SampleController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller // 화면, 데이터 제공
 @Log4j2
 public class SampleController {
@@ -13,5 +16,11 @@ public class SampleController {
     // 부트에서는 화면을 타임리프 사용, 확장자 - .html 동일함
     public void hello(Model model) {
         model.addAttribute("msg", "hello world!");
+    }
+
+    @GetMapping("/ex/ex1")
+    public void ex1(Model model) {
+        List<String> list = Arrays.asList("a", "b", "c");
+        model.addAttribute("list", list);
     }
 }
