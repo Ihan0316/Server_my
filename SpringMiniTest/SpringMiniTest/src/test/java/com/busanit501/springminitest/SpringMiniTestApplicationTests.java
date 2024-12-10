d0f77b70edbae3307f0e6e36414a9f3968ab233f
“Ihan0316” <aijnjj12@gmail.com>
2024-12-10 11:34:36 +0900

    미니실습

---
+++ SpringMiniTest/SpringMiniTest/src/test/java/com/busanit501/springminitest/SpringMiniTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busanit501.springminitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMiniTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
