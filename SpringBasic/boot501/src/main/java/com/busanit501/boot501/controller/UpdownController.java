c857a8d355dbac18254fc1886a14e720cbbcf4c7
“Ihan0316” <aijnjj12@gmail.com>
2024-12-20 13:58:22 +0900

    24/12/20 5교시

--- SpringBasic/boot501/src/main/java/com/busanit501/boot501/controller/UpdownController.java
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/controller/UpdownController.java
@@ -1,6 +1,7 @@
 package com.busanit501.boot501.controller;
 
 import com.busanit501.boot501.dto.upload.UploadFileDTO;
+import com.busanit501.boot501.dto.upload.UploadResultDTO;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.log4j.Log4j2;
 import net.coobird.thumbnailator.Thumbnailator;
@@ -14,6 +15,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -31,10 +34,15 @@ public class UpdownController {
     @Tag(name = "파일 등록 post",
             description = "멀티파트 타입 형식 이용해서, post 형식으로 업로드테스트")
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public String upload(UploadFileDTO uploadFileDTO) {
+    public List<UploadResultDTO> upload(UploadFileDTO uploadFileDTO) {
         log.info("UpdownController uploadFileDTO 내용 확인: "+uploadFileDTO);
 
         if(uploadFileDTO.getFiles() != null && uploadFileDTO.getFiles().size() > 0){
+
+            // 서버로부터 전달 받은 이미지 파일 임시 목록 저장소
+            // 추가1
+            final List<UploadResultDTO> list = new ArrayList<>();
+
             uploadFileDTO.getFiles().forEach(multipartFile -> {
                 log.info("UpdownController multipartFile.getOriginalFilename() 실제 파일 이름 확인 : "+multipartFile.getOriginalFilename());
                 String originName = multipartFile.getOriginalFilename();
@@ -44,6 +52,9 @@ public class UpdownController {
 
                 // savePath -> /Users/ihanjo/Documents/K-Digital/Upload/SpringTest/UUID임시생성문자열_파일명
                 Path savePath = Paths.get(uploadPath,uuid+"_"+originName);
+
+                // 추가2. 이미지 여부
+                boolean image = false;
 
 
                 //화면 -> 서버, 이미지 파일을 받았고,
@@ -60,6 +71,10 @@ public class UpdownController {
                     // 작은 이미지로 변환 해서, 저장,
                     // 작은 이미지라는 표시, 이름 앞에 s_ 이런식으로 이름을 변경.
                     if(Files.probeContentType(savePath).startsWith("image")){
+
+                        // 추가 4, 이미지 상태 업데이트
+                        image = true;
+
                         // 새로운 파일을 생성. 기존 원본 이미지 -> 작은 이미지
                         File thumbFile = new File(uploadPath,"s_"+ uuid+"_"+originName);
                         // 작은 이미지 변환 도구 이용해서, 축소 작업.
@@ -70,12 +85,21 @@ public class UpdownController {
                 {
                     e.printStackTrace();
                 }
+                // 추가 5, 이미지 임시 저장소 목록 추가 작업.
+                list.add(UploadResultDTO.builder()
+                        .uuid(uuid)
+                        .fileName(originName)
+                        .img(image)
+                        .build());
 
-            });
-        }
+            });// end forEach
+
+            // 추가 6, 리스트 반환
+            return list;
+        } // end if
 
         return null;
-    }
+    } // end upload
 
 
 }
