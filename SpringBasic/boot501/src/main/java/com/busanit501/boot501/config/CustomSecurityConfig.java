b892fe231d42dd0d8de64236f253337823bfcd23
“Ihan0316” <aijnjj12@gmail.com>
2024-12-30 10:12:21 +0900

    24/12/30 1교시

--- SpringBasic/boot501/src/main/java/com/busanit501/boot501/config/CustomSecurityConfig.java
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/config/CustomSecurityConfig.java
@@ -2,20 +2,34 @@ package com.busanit501.boot501.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Log4j2
 @Configuration
 @RequiredArgsConstructor
 public class CustomSecurityConfig {
+
+    // 인증 및 인가 관련 구체적인 설정
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
             log.info("CustomSecurityConfig");
 
             return http.build();
+    }
+
+    // css, js 등 정적 자원은 시큐리티 필터에서 제외하기
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        log.info("시큐리티 동작 확인 ====webSecurityCustomizer======================");
+        return (web) ->
+                web.ignoring()
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
 }
