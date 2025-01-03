ae20c4594098bc476381e93b356e8156ab49995f
“Ihan0316” <aijnjj12@gmail.com>
2025-01-03 11:23:38 +0900

    25/1/3 3교시

--- SpringBasic/boot501/src/main/java/com/busanit501/boot501/security/CustomOAuth2UserService.java
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/security/CustomOAuth2UserService.java
@@ -1,5 +1,6 @@
 package com.busanit501.boot501.security;
 
+import io.swagger.v3.oas.models.links.Link;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
@@ -9,6 +10,7 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Log4j2
@@ -29,9 +31,24 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         OAuth2User user = super.loadUser(userRequest);
         Map<String, Object> paramMap = user.getAttributes();
 
+        // 소셜 정보 출력 확인용
         paramMap.forEach((key, value) -> log.info(key + " : " + value));
 
+        String email = null;
+        switch (clientName) {
+            case "kakao":
+                email = getKakaoEmail(paramMap);
+                break;
+        }
 
         return user;
+    }
+
+    private String getKakaoEmail(Map<String, Object> params) {
+        Object value = params.get("kakao_account");
+        LinkedHashMap accountMap = (LinkedHashMap) value;
+        String email = (String) accountMap.get("email");
+        log.info("email : " + email);
+        return email;
     }
 }
